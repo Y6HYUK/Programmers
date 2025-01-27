8cce169788fa38a9f1216b40a2895f3fc17a6b0f
Junhyuk Yang <121471517+Y6HYUK@users.noreply.github.com>
2025-01-27 17:00:58 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 6.70 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,21 +1,12 @@
 def solution(s):
-    A = [-1] # 맨 처음 나오는 값 Default : -1
-    B = [] # 그 이후에 이어지는 단어들을 저장하기 위한 리스트
-    for i in range(1,len(s)):
-        B.append(s[i-1])
-        #print(B)
-        
-        if s[i] not in B:
-            A.append(-1)
+    answer = []
+    dic = {}
+    for i in range(len(s)):
+        if s[i] not in s[:i]:
+            answer.append(-1)
         else:
-            if B.count(s[i]) > 1: # 앞에 중복되는 단어가 2개 이상이면
-                # rindex를 사용하기 위해 리스트B를 문자열로 변환
-                New_B = ''.join(B)
-                A.append(i-New_B.rindex(s[i]))
-            else:
-                A.append(i-B.index(s[i]))
-          
-    #print(A)        
+            answer.append(i - dic[s[i]])
         
-    answer = A
+        dic[s[i]] = i
+        
     return answer
