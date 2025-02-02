c3793ee2c619235df84c3f24e8bcaef20441cfae
Junhyuk Yang <121471517+Y6HYUK@users.noreply.github.com>
2025-02-03 00:59:27 +0900

    [level 1] Title: 문자열 나누기, Time: 4.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,31 +1,27 @@
 def solution(s):
     answer = []
-    # 남은 문자열 s가 존재하는 동안 반복
-    while len(s) > 0:
-        x = s[0]
+    while len(s) > 0: # 문자열 s의 길이가 0이 될때까지
+        x = s[0] # 맨 처음 문자열
         count_x = 0
         count_y = 0
-        found = False  # 분리 조건이 만족되었는지 플래그로 관리
+        ### 마지막 해결책에서 len(s) == 1 만으로는 조건을 만족시키지 못함 ###
+        ### 조건을 확인할 수 있는 변수가 필요함 ###
+        disting = False
         
-        # s의 각 문자를 인덱스와 함께 순회
-        for idx, ch in enumerate(s):
-            if ch == x:
+        for i in s:
+            if i == x:
                 count_x += 1
             else:
                 count_y += 1
-            
-            if count_x == count_y:
-                # 조건 만족 시, s의 처음부터 (idx+1)까지가 하나의 부분 문자열
-                answer.append(s[:idx+1])
-                # s를 남은 부분으로 갱신
-                s = s[idx+1:]
-                found = True
+                
+            if count_x == count_y: # 횟수가 같아지면 문자열 분리
+                answer.append(s[:count_x+count_y])
+                s = s[count_x+count_y:] # 문자열 분리
+                disting = True
                 break
         
-        # for 루프를 끝까지 돌았는데 조건을 만족하지 못한 경우
-        if not found:
+        if disting == False: # 만약 마지막에 문자열이 1개만 남는다면, 처리해주기
             answer.append(s)
             break
-    
-    # 최종적으로 answer에 저장된 부분 문자열 개수를 반환
+            
     return len(answer)
